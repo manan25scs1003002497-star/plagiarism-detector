e357df8f323a56b0e696014ee6c7813932e9ee02
MANAN CHITTORIA <manan.25scs1003002497@iilm.edu>
2026-03-29 18:55:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+def check_plagiarism(text1, text2):
+    documents = [text1, text2]
+    
+    tfidf = TfidfVectorizer().fit_transform(documents)
+    similarity = cosine_similarity(tfidf[0:1], tfidf[1:2])
+    
+    return round(float(similarity[0][0]) * 100, 2)
