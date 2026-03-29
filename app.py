a42c32b9254d985e68671b69e2531a856c568e9e
MANAN CHITTORIA <manan.25scs1003002497@iilm.edu>
2026-03-29 18:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, request
+from utils import check_plagiarism
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    
+    if request.method == "POST":
+        text1 = request.form["text1"]
+        text2 = request.form["text2"]
+        
+        result = check_plagiarism(text1, text2)
+    
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
